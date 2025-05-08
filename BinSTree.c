22c77b3f9c963fb6ef532be32bac4f449f893b2b
Jett <fawwazna26@gmail.com>
2025-05-08 07:54:30 +0700

    traversal beres

--- BinSTree.c
+++ BinSTree.c
@@ -26,3 +26,29 @@ boolean IsEmpty (BinTree P) {
     /* Mengirimkan true jika BinTree KOSONG */
     return (P == Nil);
 } /* IsEmpty */
+
+/*Traversal*/
+
+void PreOrder (BinTree P) {
+    if (P != Nil) {
+        printf("%c ", GetAkar(P));
+        PreOrder(GetLeft(P));
+        PreOrder(GetRight(P));
+    }
+}
+
+void InOrder (BinTree P) {
+    if (P != Nil) {
+        InOrder(GetLeft(P));
+        printf("%c ", GetAkar(P));
+        InOrder(GetRight(P));
+    }
+} /* InOrder */
+
+void PostOrder (BinTree P) {
+    if (P != Nil) {
+        PostOrder(GetLeft(P));
+        PostOrder(GetRight(P));
+        printf("%c ", GetAkar(P));
+    }
+} /* PostOrder */
